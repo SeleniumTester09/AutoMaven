4966f2d75dde4eaf5b81cd0c3853d0c69766cba1
w8 <w8@Balas>
2018-03-31 11:47:57 +0530

    C1

---
+++ src/main/java/MavenDemo/AutoMaven/App.java
@@ -0,0 +1,16 @@
+package MavenDemo.AutoMaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+@Test
+public class App 
+{
+    public  void printHello()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenDemo/AutoMaven/TestLogin.java
@@ -0,0 +1,42 @@
+package MavenDemo.AutoMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		
+		  System.setProperty("webdriver.chrome.driver", "C:/eclipse\\chromedriver.exe");
+		   driver = new ChromeDriver();
+		
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		System.out.println("Quitting the driver");
+		driver.quit();
+	}
+	
+	
+	@Test(priority=1)
+	public void navigateURL(){
+		
+		driver.get("http://google.com");
+		
+	}
+	
+	@Test(priority=2)
+	public void validateTitle(){
+		
+		Assert.assertEquals("Gmail", driver.getTitle());
+	}
+
+}
